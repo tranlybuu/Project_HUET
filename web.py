8fa4ccbe515453cc78ed72bf79362416b0f51db4
tranlybuu <lybuu9117@gmail.com>
2021-01-13 00:01:25 +0700

    Buu

--- web.py
+++ web.py
@@ -9,10 +9,8 @@ import re
 #Hàm đọc nội dung trang web chỉ định
 #Kết quả trả về là 1 văn bản dạng chuỗi
 def doc_noi_dung(url):
-    #Gửi yêu cầu truy cập url
-    page = requests.get(url)
-    #Lấy code html của trang web được trả về theo url
-    content = BeautifulSoup(page.content, 'html.parser')
+    page = requests.get(url)    #Gửi yêu cầu truy cập url
+    content = BeautifulSoup(page.content, 'html.parser')       #Lấy code html của trang web được trả về theo url
     return content
 
 #Hàm lấy các đường link web trong nội dung đọc về
@@ -20,19 +18,18 @@ def doc_noi_dung(url):
 def lay_cac_duong_link(content):
     url_list = []
     result = []
-    raw = content.find_all("a")
+    raw = content.find_all("a")     #Lọc các thẻ <a>
     for item in raw:
-        link = item.get("href")
+        link = item.get("href")     #Lấy ra các đường dẫn trong href
         url_list.append(link)
-    for item in url_list:
-        item = str(item)
-        if (item.find("http", 0, 4)):
-            if (item.find("java", 0, 4)):
-                if item.find("html", (len(item) - 4), len(item)):
-                    if (item.find("#", 0, 4)):
-                        if (item.find("None", 0, 4)):
-                            if len(item) > 2:
-                                result.append(item)
+    for item in url_list:                                           ###
+        item = str(item)                                              ###
+        if (item.find("http", 0, 4)):                                   ###    Lọc các đường dẫn trong url_list
+            if (item.find("java", 0, 4)):                                 ##     Giữ lại các đường dẫn
+                    if (item.find("#", 0, 4)):                            ##    như https://... và /vn/..
+                        if (item.find("None", 0, 4)):                   ###      Rồi thêm vào list Result
+                            if len(item) > 2:                         ###
+                                result.append(item)                 ###
         if not(item.find("http", 0, 4)):
             result.append(item)
     return result
@@ -41,7 +38,7 @@ def lay_cac_duong_link(content):
 #Hàm kiểm tra tính hợp lệ của 1 đường link
 #Kết quả trả về: True nếu hợp lệ / False nếu không hợp lệ
 def kiem_tra_link(link):
-    check = re.search("^http", link)
+    check = re.search("^http", link)    #Kiểm tra link với các kí tự bắt đầu là http
     try:
         if link == check.string:
             return True
@@ -52,6 +49,6 @@ def kiem_tra_link(link):
 #Hàm chỉnh sửa đường link nếu đường link không đầy đủ
 #Kết quả trả về là 1 đường link đầy đủ
 def chinh_sua_link(url,item):
-    item = item.split(" ")      #Loại bỏ khoảng trống 2 đầu chuỗi
-    url_new= str(url) + item[0]
+    item = item.split(" ")          #Loại bỏ khoảng trống 2 đầu chuỗi
+    url_new= str(url) + item[0]     #Thêm https://... vào
     return url_new
